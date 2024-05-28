32a7bf64fe2e3345aefa2a65d0820c6b134054ae
KamilGarbacki <kamil.garbacki06@gmail.com>
2024-05-29 00:07:25 +0200

    Initial commit

---
+++ src/test/java/com/kamilgarbacki/Travel_app/TravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilgarbacki.Travel_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
